3e196b0641d93975ce02355fb757bd9e5ba0b6b2
Gabriel Freitas <gabrielfreitas@usp.br>
2018-10-20 05:11:12 -0300

    [ADD] Discrete Random Walk Visualization

---
+++ monte_carlo/discreteRandomWalk_vis.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import random
+import time
+import numpy as np
+
+def discrete_random_walk(n=1):
+    x, y = 0, 0
+    for i in range(n):
+        (dx, dy) = random.choice([(0, 1),
+                                  (0,-1),
+                                  (1, 0),
+                                  (-1,0)])
+        x += dx
+        y += dy
+    return (x,y)
+
+for k in range(25):
+    plt.close()
+    plt.figure(figsize=(16,9))
+    plt.xticks(range(-100,101))
+    plt.yticks(range(-100,101))
+    plt.grid(alpha=0.2)
+    plt.scatter(0,0,color='black', s=50, marker='+')
+
+    origin = [0],[0]
+    pos_x = 0
+    pos_y = 0
+
+    steps = 50
+
+    for i in range(steps):
+        print(i)
+        x, y = discrete_random_walk()
+
+        pos_x += x
+        pos_y += y
+
+        plt.quiver(*origin, x, y, 
+                    angles='xy', 
+                    scale_units='xy', 
+                    scale=1, 
+                    color='#42BEBA',
+                    alpha=0.3)
+
+        origin = np.add(origin[0],x),np.add(origin[1],y)
+
+        plt.title(f'Passos = {i} | Distância até origem (em quadras): {abs(pos_x)+abs(pos_y)}')
+        plt.pause(0.001)
+
+    origin = [0],[0]
+    plt.title(f'Passos = {steps} | Distância até origem (em quadras): {abs(pos_x)+abs(pos_y)}')
+    plt.quiver(*origin, pos_x, pos_y, 
+                angles='xy', 
+                scale_units='xy', 
+                scale=1, 
+                color='darkgray',
+                alpha=0.7)
+    if (pos_x == 0) | (pos_y == 0):
+        plt.scatter(0,0,color='red', s=50, marker='+')
+    else: 
+        plt.scatter(0,0,color='black', s=50, marker='+')
+    plt.pause(0.05)
+    time.sleep(2)
+
+input()
