378e203e29a7f148757951e7ef3ad34955ecaec0
Gabriel Freitas <gabrielfreitas@usp.br>
2018-11-07 04:09:51 -0200

    :sparkles: Huge refactor on entire code.

--- simulations/closed_recipient.py
+++ simulations/closed_recipient.py
@@ -1,31 +1,37 @@
 import numpy as np
-#import itertools
+import itertools
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(6,6))
 plt.grid()
-points, = ax.plot(0,'o')
+points = ax.plot(0,'o')[0]
 
-ax.set_xlim(0,2)
+ax.set_xlim(0,1)
 ax.set_ylim(0,1)
-ax.set_xticks(np.linspace(0,2,41))
+ax.set_xticks(np.linspace(0,1,21))
 ax.set_yticks(np.linspace(0,1,21))
 for tick in ax.get_xticklabels(): tick.set_rotation(50)
 
 def get_initial_conditions():
 
-	n_particles = 100
+	n_particles = 50
 	velocity    = 5
 
-	x   = np.random.uniform(0,2,n_particles)
+	x   = np.random.uniform(0,1,n_particles)
 	y   = np.random.uniform(0,1,n_particles)
 	phi = np.random.uniform(0,2*np.pi,n_particles)
 
 	v_x = velocity*np.cos(phi)
 	v_y = velocity*np.sin(phi)
 
-	return x, y, v_x, v_y
+	x_diffs = np.apply_along_axis(lambda f: f[0]-f[1], 1, np.array(list(itertools.permutations(x, 2))))
+	y_diffs = np.apply_along_axis(lambda f: f[0]-f[1], 1, np.array(list(itertools.permutations(y, 2))))
+	dists = np.split(np.insert(np.sqrt(y_diffs**2 + x_diffs**2), range(0,n_particles**2,n_particles), 0), n_particles)
+
+	system_initial_state = np.hstack((np.dstack((x, y, v_x, v_y, phi))[0], dists))
+
+	return n_particles, system_initial_state
 
 def update(data):
 	points.set_xdata(data[0])
@@ -34,32 +40,42 @@ def update(data):
 	points.set_markersize(6)
 	return points,
 
-def generate_points():
+def calc_dif(pos):
+	return pos[0]-pos[1]
 
-	delta_t = 0.001
+def update_xy(states):
+	delta_t = 0.0005
+	states[:,0] = states[:,0] + states[:,2]*delta_t
+	states[:,1] = states[:,1] + states[:,3]*delta_t
 
-	R = 0.0025
+def update_dists(states):
+	x_diffs = np.apply_along_axis(calc_dif, 1, np.array(list(itertools.permutations(states[:,0], 2))))
+	y_diffs = np.apply_along_axis(calc_dif, 1, np.array(list(itertools.permutations(states[:,1], 2))))
+	states[:,5:] = np.split(np.insert(np.sqrt(y_diffs**2 + x_diffs**2),range(0,len(states)**2,len(states)),0),len(states))
 
-	x, y, v_x, v_y = get_initial_conditions()
+def check_wall_collision(states):
+	x_wall_collision = np.argwhere((states[:,0]>=1)|(states[:,0]<=0)).T
+	y_wall_collision = np.argwhere((states[:,1]>=1)|(states[:,1]<=0)).T
+	states[:,2][x_wall_collision] = -states[:,2][x_wall_collision]
+	states[:,3][y_wall_collision] = -states[:,3][y_wall_collision]
 
+def check_ball_collision(states):
+	return
+
+def make_simulation():
+
+	N, states = get_initial_conditions()
+	
 	while True:
 
-		x, y = x + v_x*delta_t, y + v_y*delta_t
+		check_ball_collision(states)
+		check_wall_collision(states)
 
-		#x_pairs = np.array(list(itertools.combinations(x, 2)))
-		#y_pairs = np.array(list(itertools.combinations(y, 2)))
-		#x_diff  = np.apply_along_axis(lambda k:k[0]-k[1],1,x_pairs)
-		#y_diff  = np.apply_along_axis(lambda k:k[0]-k[1],1,y_pairs)
-		#dist    = np.sqrt(x_diff**2 + y_diff**2)
-		#ball_collision = np.where(dist <= 2*R)
+		update_xy(states)
+		update_dists(states)
 
-		x_wall_collision = np.argwhere((x>=2)|(x<=0)).T
-		y_wall_collision = np.argwhere((y>=1)|(y<=0)).T
-		v_x[x_wall_collision] = -v_x[x_wall_collision]
-		v_y[y_wall_collision] = -v_y[y_wall_collision]
+		yield [states[:,0], states[:,1]]
 
-		yield [x, y]
-
-ani = animation.FuncAnimation(fig, update, generate_points, interval=5, blit=True)
+ani = animation.FuncAnimation(fig, update, make_simulation, interval=5, blit=True)
 
 plt.show()
